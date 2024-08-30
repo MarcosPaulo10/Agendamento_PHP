0e28c73ac32e130a68e66f84509dcfe786effb77
Marcos Paulo Nogueira Mendes <mpnm10@hotmail.com>
2024-08-29 21:49:23 -0300

    Configuração database.php

--- app/model/database.php
+++ app/model/database.php
@@ -1,4 +1,8 @@
 <?php
+
+    include_once __DIR__ . '/../../config/dbconfig.php'
+
+
     class database {
         private $servername;
         private $username;
@@ -12,6 +16,22 @@
             $this->password = DB_PASSWORD;
             $this->dbname = DB_NAME;
 
+        } 
+        
+        fuction connect () {
+            $this->conn = new mysql ($this->servername, $this->username, $this->password, $this->dbname);
+
+            if ($this->conn->connect_error) {
+                die("Falha na conexão com o banco de dados: " . $this->conn->connect_error);
+            }
+
+            return $this->conn;
+        }
+
+        fuction closeconnection () {
+            if ($this->conn) {
+                $this->conn->close();
+            }
         }
 
     }
